5cc349a1b13aef589bf7726a56fac2f45782a073
Victor Moreira <victormoreira176@hotmail.com>
2021-03-22 14:28:33 -0300

    :sparkles: adding keepalive route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 import discord from './config/discordConfig';
 import youtube from './config/youtubeConfig';
 import { DiscordModule } from './discord/discord.module';
@@ -9,7 +10,7 @@ import { DiscordModule } from './discord/discord.module';
     ConfigModule.forRoot({ isGlobal: true, load: [discord, youtube] }),
     DiscordModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  app.use(helmet);
+  app.use(helmet());
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
